6c23c6078c715ce629818472446f08effd17535b
Ryan Neubauer <rjneub09@gmail.com>
2023-02-22 13:03:19 -0600

    Added basic message function.

--- 3B-main.py
+++ 3B-main.py
@@ -12,6 +12,18 @@ client = discord.Client(intents=discord.Intents.default())
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('Hi'):
+        channel = message.channel
+        await channel.send('Say hello!')
+
+        def check(m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for('message', check=check)
+        await channel.send(f'Hello {msg.author}!')
+
 client.run(TOKEN)
 
 
