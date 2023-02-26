d8cc5ac6677109dba270218272539094fef9036f
Ryan Neubauer <rjneub09@gmail.com>
2023-02-26 12:13:20 -0600

    Started voice commands

--- cogs/voice.py
+++ cogs/voice.py
@@ -3,12 +3,29 @@ import discord
 from discord.ext import commands
 
 
-class voice():
-    def __init__(self,bot):
+class voice(commands.Cog):
+    def __init__(self, bot):
         self.bot = bot
-    async def joinVC(self,ctx):
-        channel = ctx.author.voice_channel
-        await self.bot.join_voice_channel(channel)
+
+    @commands.command()    
+    async def join(self, ctx):
+        if ctx.voice_client != None:
+            await ctx.move_to(ctx.voice.author.voice.channel)
+        else:
+            channel = ctx.author.voice.channel
+            await channel.connect()
+
+    @commands.command()
+    async def isoccupied(self, ctx):
+        if ctx.voice_client.is_connected():
+            await ctx.send("Bot is occupied")
+
+    @commands.command()
+    async def leave(self, ctx):
+        if ctx.voice_client != None:
+            await ctx.voice_client.disconnect()
+        else:
+            await ctx.send("Bot is not in a channel...")
 
 async def setup(bot):
     await bot.add_cog(voice(bot))
