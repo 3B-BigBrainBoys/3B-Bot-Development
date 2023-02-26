2038567c220973b6902709904dacec7a727ddb29
Ryan Neubauer <rjneub09@gmail.com>
2023-02-26 13:18:15 -0600

    mp3 push

--- cogs/voice.py
+++ cogs/voice.py
@@ -1,6 +1,8 @@
 
 import discord
 from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
 
 
 class voice(commands.Cog):
@@ -17,8 +19,10 @@ class voice(commands.Cog):
 
     @commands.command()
     async def isoccupied(self, ctx):
-        if ctx.voice_client.is_connected():
-            await ctx.send("Bot is occupied")
+        if ctx.voice_client != None:
+            await ctx.send("Bot is in a channel")
+        else:
+            await ctx.send("Bot is not connected to a channel")
 
     @commands.command()
     async def leave(self, ctx):
@@ -26,6 +30,11 @@ class voice(commands.Cog):
             await ctx.voice_client.disconnect()
         else:
             await ctx.send("Bot is not in a channel...")
+
+    @commands.command(brief="Plays an mp3")
+    async def play(self, ctx):
+        source = FFmpegPCMAudio('song.mp3')
+        player = ctx.voice_client.play(source)
 
 async def setup(bot):
     await bot.add_cog(voice(bot))
