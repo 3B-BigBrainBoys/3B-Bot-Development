25dd36c3eea384d2ab07b376c0232397ba158432
Ryan Neubauer <rjneub09@gmail.com>
2023-04-05 13:32:05 -0500

    Modifying the 3B-main.py

--- 3B-main.py
+++ 3B-main.py
@@ -10,25 +10,21 @@ from discord import app_commands
 load_dotenv()
 TOKEN = os.getenv('EXPERIMENTAL_TOKEN')
 
-class MyBot(commands.Bot):
-    super().__init__(
-        command_prefix = '$',
-        intents = discord.Intents.all(),
-        application_id = 123
-    )
+intents = discord.Intents.all()
+bot = discord.Bot(intents=intents)
+tree = app_commands.CommandTree(bot)
 
-    # Bot startup
-    async def on_ready():
-        print(f'{bot.user} has connected to Discord!')
-        print(f'Running on version: {discord.__version__}')
-        # For each cog in the /cog directory, load the cog
+# Bot startup
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    print(f'Running on version: {discord.__version__}')
+    # For each cog in the /cog directory, load the cog
 
-    async def setup_hook(self):
-        for filename in os.listdir('./cogs'):
-            if filename.endswith('.py'):
-                await bot.load_extension(f"cogs.{filename[:-3]}")
-        await bot.tree.sync(guild = discord.Object(id = 456))
+async def setup_hook(self):
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+    await bot.tree.sync(guild = discord.Object(id = 456))
 
-bot = MyBot()
 bot.run(TOKEN)
 
